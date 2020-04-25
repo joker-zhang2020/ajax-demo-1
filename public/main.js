2ea3a681526a81ea7fcd8ca13d99234e747cb9b9
joker_ <zhangzhe_sdu@icloud.com>
2020-04-25 20:03:18 +0800

    init

---
+++ public/main.js
@@ -0,0 +1,86 @@
+console.log("我是main.js ");
+let n = 1;
+
+getPage.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", `/page${n + 1}`);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const array = JSON.parse(request.response);
+      array.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = item.id;
+        xxx.appendChild(li);
+      });
+      n += 1;
+    }
+  };
+  request.send();
+};
+
+getJSON.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/5.json");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const object = JSON.parse(request.response);
+      myName.textContent = object.name;
+    }
+  };
+  request.send();
+};
+
+getXML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/4.xml");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const dom = request.responseXML;
+      const text = dom.getElementsByTagName("warning")[0].textContent;
+      console.log(text.trim());
+    }
+  };
+  request.send();
+};
+
+getHTML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/3.html");
+  request.onload = () => {
+    const div = document.createElement("div");
+    div.innerHTML = request.response;
+    document.body.appendChild(div);
+  };
+  request.onerror = () => {};
+  request.send();
+};
+
+getCSS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/style.css");
+  request.onload = () => {
+    console.log("1");
+    const style = document.createElement("style");
+    style.innerHTML = request.response;
+    document.head.appendChild(style);
+  };
+  request.onerror = () => {
+    console.log("0");
+  };
+  request.send();
+};
+
+getJS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/2.js");
+  request.onload = () => {
+    console.log(request.response);
+    console.log("2");
+    const script = document.createElement("script");
+    script.innerHTML = request.response;
+    document.body.appendChild(script);
+  };
+  request.error = () => {};
+  request.send();
+};
